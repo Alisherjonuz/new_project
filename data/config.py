cc7a5edea64b3e27c2b4bad2be17c1e378ba0c44
Alisherjonuz <108214363+Alisherjonuz@users.noreply.github.com>
2023-07-11 12:59:47 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -5,6 +5,6 @@
 # env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = "5840338658:AAHC38CvnC8-b4tiE3JhwJv0S5nyKo6XmbE" # Bot toekn
+BOT_TOKEN = "Token" # Bot toekn
 ADMINS = [5069753238]  # adminlar ro'yxati
 IP = 'localhost'  # Xosting ip manzili
